ebf5a1fbcf493255c39003adc757b1f2085d8518
KumarSChandra <58375738+KumarSChandra@users.noreply.github.com>
2020-05-09 01:59:26 -0700

    starting gui!
    doesn't run at the moment

--- src/Battle.java
+++ src/Battle.java
@@ -1,6 +1,4 @@
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Scanner;
 import javax.swing.*;
 import java.awt.*;
@@ -9,66 +7,26 @@ import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import javax.imageio.*;
 
-public class Main extends JFrame {
+public class Battle {
     public static int P1numberOfFaintedMons=0;
     public static int P2numberOfFaintedMons=0;
-    
-    private JFrame frame;
-    private JPanel panel, display, movePanel, switchPanel;
-    private JLabel name1, name2, image1, image2, attack, switchOut;
-    private JButton[] movesButtons = new JButton[4];
-    private JButton[] switchesButtons = new JButton[6];
-    private JTextArea text;
 
-    public Main() {
+	private JFrame frame;
+	private JPanel panel, display, movePanel, switchPanel;
+	private JLabel name1, name2, image1, image2, attack, switchOut;
+	private Button[] moves = new Button[4];
 
-        EaseOfUse ez=new EaseOfUse();
-        Scanner kboard=new Scanner(System.in);
-        Calculator calc=new Calculator();
+	private Button[] switches = new Button[6];
+	private JTextArea text;
+
+
+
+
+
+	public Battle(Player p1, Player p2) {
+
         String filepath = "PokemonTitleScreen.wav";
         PlayMusic musicObject = new PlayMusic();
-        musicObject.playMusic(filepath);
-        BasicPokemon[] p1mons=new BasicPokemon[6];
-        BasicPokemon[] p2mons=new BasicPokemon[6];
-        int[][] givenMoves = new int[6][4];
-
-        ez.print("P1) Select your first pokemon's dex number");
-        p1mons[0]=new BasicPokemon(kboard.nextInt());
-
-        ez.print("P1) Select your second pokemon's dex number");
-        p1mons[1]=new BasicPokemon(kboard.nextInt());
-        ez.print("P1) Select your third pokemon's dex number");
-        p1mons[2]=new BasicPokemon(kboard.nextInt());
-        ez.print("P1) Select your fourth pokemon's dex number");
-        p1mons[3]=new BasicPokemon(kboard.nextInt());
-        ez.print("P1) Select your fifth pokemon's dex number");
-        p1mons[4]=new BasicPokemon(kboard.nextInt());
-        ez.print("P1) Select your sixth pokemon's dex number");
-        p1mons[5]=new BasicPokemon(kboard.nextInt());
-
-
-        ez.print("P2) Select your first pokemon's dex number");
-        p2mons[0]=new BasicPokemon(kboard.nextInt());
-        ez.print("P2) Select your second pokemon's dex number");
-        p2mons[1]=new BasicPokemon(kboard.nextInt());
-        ez.print("P2) Select your third pokemon's dex number");
-        p2mons[2]=new BasicPokemon(kboard.nextInt());
-        ez.print("P2) Select your fourth pokemon's dex number");
-        p2mons[3]=new BasicPokemon(kboard.nextInt());
-        ez.print("P2) Select your fifth pokemon's dex number");
-        p2mons[4]=new BasicPokemon(kboard.nextInt());
-        ez.print("P2) Select your sixth pokemon's dex number");
-        p2mons[5]=new BasicPokemon(kboard.nextInt());
-        givenMoves[0]= new int[]{14,370,609,1};
-        givenMoves[1]= new int[]{6,7,8,9};
-        givenMoves[2]= new int[]{10,11,12,13};
-        givenMoves[3]= new int[]{14,15,16,17};
-        givenMoves[4]= new int[]{18,19,20,21};
-        givenMoves[5]= new int[]{22,23,24,25};
-
-        Player p1=new Player(p1mons,givenMoves);
-        Player p2=new Player(p2mons,givenMoves,p1);
-        p1.setOpposingPlayer(p2);
         frame = new JFrame();
     	panel = new JPanel(new GridBagLayout());
     	display = new JPanel();
@@ -133,10 +91,13 @@ public class Main extends JFrame {
     	GridLayout l = new GridLayout(1, 4);
     	movePanel.setLayout(l);
     	for(int move = 0; move < moves.length; move++) {
-    		moves[move] = new JButton("" + (move+1));
-    		moves[move].addActionListener(){
-
-            }
+    		moves[move] = new Button("Move"+(move+1),move+1);
+//    		moves[move].addActionListener(new ActionListener() {
+//                @Override
+//                public void actionPerformed(ActionEvent e) {
+//
+//                }
+//            });
     		movePanel.add(moves[move]);
     	}
     	c.gridx = 0;
@@ -163,8 +124,13 @@ public class Main extends JFrame {
     	l = new GridLayout(1, 6);
     	switchPanel.setLayout(l);
     	for(int mon = 0; mon < switches.length; mon++) {
-    		switches[mon] = new JButton("" + (mon+1));
-    		switches[mon].addActionListener(this);
+    		switches[mon] = new Button("Move" + (mon+1),mon+1);
+//    		switches[mon].addActionListener(new ActionListener() {
+//                @Override
+//                public void actionPerformed(ActionEvent e) {
+//
+//                }
+//            });
     		switchPanel.add(switches[mon]);
     	}
     	c.gridx = 0;
@@ -215,173 +181,189 @@ public class Main extends JFrame {
 //    System.out.println(
 ////            read.orderMoves(
 //            (read.formatShowDownLearnSets(copy).replace("\t","")));
-
-    	new Main();
-
         EaseOfUse ez=new EaseOfUse();
         Scanner kboard=new Scanner(System.in);
         Calculator calc=new Calculator();
-//        String filepath = "PokemonTitleScreen.wav";
-//        PlayMusic musicObject = new PlayMusic();
-//        musicObject.playMusic(filepath);
-//        BasicPokemon[] p1mons=new BasicPokemon[6];
-//        BasicPokemon[] p2mons=new BasicPokemon[6];
-//        int[][] givenMoves = new int[6][4];
-//
-//        ez.print("P1) Select your first pokemon's dex number");
-//        p1mons[0]=new BasicPokemon(kboard.nextInt());
-//
-//        ez.print("P1) Select your second pokemon's dex number");
-//        p1mons[1]=new BasicPokemon(kboard.nextInt());
-//        ez.print("P1) Select your third pokemon's dex number");
-//        p1mons[2]=new BasicPokemon(kboard.nextInt());
-//        ez.print("P1) Select your fourth pokemon's dex number");
-//        p1mons[3]=new BasicPokemon(kboard.nextInt());
-//        ez.print("P1) Select your fifth pokemon's dex number");
-//        p1mons[4]=new BasicPokemon(kboard.nextInt());
-//        ez.print("P1) Select your sixth pokemon's dex number");
-//        p1mons[5]=new BasicPokemon(kboard.nextInt());
-//
-//
-//        ez.print("P2) Select your first pokemon's dex number");
-//        p2mons[0]=new BasicPokemon(kboard.nextInt());
-//        ez.print("P2) Select your second pokemon's dex number");
-//        p2mons[1]=new BasicPokemon(kboard.nextInt());
-//        ez.print("P2) Select your third pokemon's dex number");
-//        p2mons[2]=new BasicPokemon(kboard.nextInt());
-//        ez.print("P2) Select your fourth pokemon's dex number");
-//        p2mons[3]=new BasicPokemon(kboard.nextInt());
-//        ez.print("P2) Select your fifth pokemon's dex number");
-//        p2mons[4]=new BasicPokemon(kboard.nextInt());
-//        ez.print("P2) Select your sixth pokemon's dex number");
-//        p2mons[5]=new BasicPokemon(kboard.nextInt());
-//        givenMoves[0]= new int[]{14,370,609,1};
-//        givenMoves[1]= new int[]{6,7,8,9};
-//        givenMoves[2]= new int[]{10,11,12,13};
-//        givenMoves[3]= new int[]{14,15,16,17};
-//        givenMoves[4]= new int[]{18,19,20,21};
-//        givenMoves[5]= new int[]{22,23,24,25};
-//
-//        Player p1=new Player(p1mons,givenMoves);
-//        Player p2=new Player(p2mons,givenMoves,p1);
-//        p1.setOpposingPlayer(p2);
-//        System.out.println("The match has begun!");
-        boolean gameNotOver=true;
-        while(gameNotOver) {
+        BasicPokemon[] p1mons=new BasicPokemon[6];
+        BasicPokemon[] p2mons=new BasicPokemon[6];
+        int[][] givenMoves = new int[6][4];
 
-            boolean p1WillSwitch=false;
+
+
+
+
+        ez.print("P1) Select your first pokemon's dex number");
+        p1mons[0]=new BasicPokemon(kboard.nextInt());
+
+        ez.print("P1) Select your second pokemon's dex number");
+        p1mons[1]=new BasicPokemon(kboard.nextInt());
+        ez.print("P1) Select your third pokemon's dex number");
+        p1mons[2]=new BasicPokemon(kboard.nextInt());
+        ez.print("P1) Select your fourth pokemon's dex number");
+        p1mons[3]=new BasicPokemon(kboard.nextInt());
+        ez.print("P1) Select your fifth pokemon's dex number");
+        p1mons[4]=new BasicPokemon(kboard.nextInt());
+        ez.print("P1) Select your sixth pokemon's dex number");
+        p1mons[5]=new BasicPokemon(kboard.nextInt());
+
+
+        ez.print("P2) Select your first pokemon's dex number");
+        p2mons[0]=new BasicPokemon(kboard.nextInt());
+        ez.print("P2) Select your second pokemon's dex number");
+        p2mons[1]=new BasicPokemon(kboard.nextInt());
+        ez.print("P2) Select your third pokemon's dex number");
+        p2mons[2]=new BasicPokemon(kboard.nextInt());
+        ez.print("P2) Select your fourth pokemon's dex number");
+        p2mons[3]=new BasicPokemon(kboard.nextInt());
+        ez.print("P2) Select your fifth pokemon's dex number");
+        p2mons[4]=new BasicPokemon(kboard.nextInt());
+        ez.print("P2) Select your sixth pokemon's dex number");
+        p2mons[5]=new BasicPokemon(kboard.nextInt());
+        givenMoves[0]= new int[]{14,370,609,1};
+        givenMoves[1]= new int[]{6,7,8,9};
+        givenMoves[2]= new int[]{10,11,12,13};
+        givenMoves[3]= new int[]{14,15,16,17};
+        givenMoves[4]= new int[]{18,19,20,21};
+        givenMoves[5]= new int[]{22,23,24,25};
+
+        Player p1=new Player(p1mons,givenMoves);
+        Player p2=new Player(p2mons,givenMoves,p1);
+        p1.setOpposingPlayer(p2);
+		Battle b= new Battle(p1,p2);
+
+
+        System.out.println("The match has begun!");
+        boolean gameNotOver=true;
+		boolean p1WillSwitch=false;
             boolean p2WillSwitch=false;
             int p1SelectedMoveIndex=0;
             int p2SelectedMoveIndex = 0;
-            int selection;
+            int moveSelection;
+
+            int switchSelection;
+            for(int i=0;i<b.moves.length;i++){
+
+            	b.moves[i].addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent e) {
+
+						b.moves[((Button)e.getSource()).num]=((Button)e.getSource());
+
+					}
+				});
+			}
+
+        while(gameNotOver) {
+
+
             boolean youShallNotPass = true;
-            while (youShallNotPass) {
-                System.out.println("Player1 what would you like to do\n1)Switch out\n2)Fight");
-                selection = kboard.nextInt();
-                if (selection == 1&&P1numberOfFaintedMons<5) {
-                    p1WillSwitch = true;
-                    youShallNotPass = false;
-                } else if (selection == 2) {
-                    System.out.println("Choose which move you want to use");
-                    for (int i = 0; i < 4; i++) {
-                        System.out.println((i + 1) + ")" + p1.currentMon.getMoves()[i]);
-                    }
-                    p1SelectedMoveIndex = kboard.nextInt() - 1;
-                    youShallNotPass = false;
-                }
-                else if(selection == 1&&P1numberOfFaintedMons>=5){
-                    ez.print("You have no available pokemon to switch in, because every other Pokemon has fainted");
-                }
-                else {
-                    System.out.println("That wasn't a valid input");
-                }
-            }
-            youShallNotPass=true;
-            selection=0;
-            while(youShallNotPass) {
-                System.out.println("Player2 what would you like to do\n1)Switch out\n2)Fight");
-                selection = kboard.nextInt();
-                if (selection == 1) {
-                    p2WillSwitch = true;
-                    youShallNotPass = false;
-                } else if (selection == 2) {
-                    System.out.println("Choose which move you want to use");
-                    for (int i = 0; i < 4; i++) {
-                        System.out.println((i + 1) + ")" + p2.currentMon.getMoves()[i]);
-                    }
-                    p2SelectedMoveIndex = kboard.nextInt() - 1;
-                    youShallNotPass = false;
-                }
-                else if(selection == 1&&P2numberOfFaintedMons>=5){
-                    ez.print("You have no available pokemon to switch in, because every other Pokemon has fainted");
-                }else {
-                    System.out.println("That wasn't a valid input");
-                }
-            }
-
-            if(p1WillSwitch==true&&p2WillSwitch==true){
-                if(p1.currentMon.getSpeed()>p2.currentMon.getSpeed()){
-                    p1.switchOut();
-                    p2.switchOut();
-                }
-                else if(p2.currentMon.getSpeed()>p1.currentMon.getSpeed()){
-                    p2.switchOut();
-                    p1.switchOut();
-                }
-                else{
-                    if(Math.random()>0.5){
-                        p1.switchOut();
-                        p2.switchOut();
-                    }
-                    else{
-                        p2.switchOut();
-                        p1.switchOut();
-                    }
-                }
-
-            }
-            else if(p1WillSwitch==true){
-                p1.switchOut();
-                p2.fight(p2SelectedMoveIndex);
-            }
-            else if(p2WillSwitch==true){
-                p2.switchOut();
-                p1.fight(p1SelectedMoveIndex);
-            }
-            else{
-
-                int x=calc.calculateWhoGoesFirst(p1,p2,p1.currentMon.getMoves()[p1SelectedMoveIndex],p2.currentMon.getMoves()[p2SelectedMoveIndex]);
-                if(x==1){
-                    p1.fight(p1SelectedMoveIndex);
-                    p2.fight(p2SelectedMoveIndex);
-                }
-                else if(x==2){
-                    p2.fight(p2SelectedMoveIndex);
-                    p1.fight(p1SelectedMoveIndex);
-                }
-                else{
-                    while(true) {
-                        ez.print("Something went wrong");
-                    }
-                }
-            }
-            if(p1.isDefeated()){
-                gameNotOver=false;
-                break;
-            }
-            if(p2.isDefeated()){
-                gameNotOver=false;
-                break;
-            }
-            if(p1.currentMon.getHealth()<=0){
-                p1.switchOut();
-                P1numberOfFaintedMons++;
-            }
-            if(p2.currentMon.getHealth()<=0){
-                p2.switchOut();
-                P2numberOfFaintedMons++;
-            }
-        }
+//            while (youShallNotPass) {
+//                System.out.println("Player1 what would you like to do\n1)Switch out\n2)Fight");
+//                selection = kboard.nextInt();
+//                if (selection == 1&&P1numberOfFaintedMons<5) {
+//                    p1WillSwitch = true;
+//                    youShallNotPass = false;
+//                } else if (selection == 2) {
+//                    System.out.println("Choose which move you want to use");
+//                    for (int i = 0; i < 4; i++) {
+//                        System.out.println((i + 1) + ")" + p1.currentMon.getMoves()[i]);
+//                    }
+//                    p1SelectedMoveIndex = kboard.nextInt() - 1;
+//                    youShallNotPass = false;
+//                }
+//                else if(selection == 1&&P1numberOfFaintedMons>=5){
+//                    ez.print("You have no available pokemon to switch in, because every other Pokemon has fainted");
+//                }
+//                else {
+//                    System.out.println("That wasn't a valid input");
+//                }
+//            }
+//            youShallNotPass=true;
+//            selection=0;
+//            while(youShallNotPass) {
+//                System.out.println("Player2 what would you like to do\n1)Switch out\n2)Fight");
+//                selection = kboard.nextInt();
+//                if (selection == 1) {
+//                    p2WillSwitch = true;
+//                    youShallNotPass = false;
+//                } else if (selection == 2) {
+//                    System.out.println("Choose which move you want to use");
+//                    for (int i = 0; i < 4; i++) {
+//                        System.out.println((i + 1) + ")" + p2.currentMon.getMoves()[i]);
+//                    }
+//                    p2SelectedMoveIndex = kboard.nextInt() - 1;
+//                    youShallNotPass = false;
+//                }
+//                else if(selection == 1&&P2numberOfFaintedMons>=5){
+//                    ez.print("You have no available pokemon to switch in, because every other Pokemon has fainted");
+//                }else {
+//                    System.out.println("That wasn't a valid input");
+//                }
+//            }
+//
+//            if(p1WillSwitch==true&&p2WillSwitch==true){
+//                if(p1.currentMon.getSpeed()>p2.currentMon.getSpeed()){
+//                    p1.switchOut();
+//                    p2.switchOut();
+//                }
+//                else if(p2.currentMon.getSpeed()>p1.currentMon.getSpeed()){
+//                    p2.switchOut();
+//                    p1.switchOut();
+//                }
+//                else{
+//                    if(Math.random()>0.5){
+//                        p1.switchOut();
+//                        p2.switchOut();
+//                    }
+//                    else{
+//                        p2.switchOut();
+//                        p1.switchOut();
+//                    }
+//                }
+//
+//            }
+//            else if(p1WillSwitch==true){
+//                p1.switchOut();
+//                p2.fight(p2SelectedMoveIndex);
+//            }
+//            else if(p2WillSwitch==true){
+//                p2.switchOut();
+//                p1.fight(p1SelectedMoveIndex);
+//            }
+//            else{
+//
+//                int x=calc.calculateWhoGoesFirst(p1,p2,p1.currentMon.getMoves()[p1SelectedMoveIndex],p2.currentMon.getMoves()[p2SelectedMoveIndex]);
+//                if(x==1){
+//                    p1.fight(p1SelectedMoveIndex);
+//                    p2.fight(p2SelectedMoveIndex);
+//                }
+//                else if(x==2){
+//                    p2.fight(p2SelectedMoveIndex);
+//                    p1.fight(p1SelectedMoveIndex);
+//                }
+//                else{
+//                    while(true) {
+//                        ez.print("Something went wrong");
+//                    }
+//                }
+//            }
+//            if(p1.isDefeated()){
+//                gameNotOver=false;
+//                break;
+//            }
+//            if(p2.isDefeated()){
+//                gameNotOver=false;
+//                break;
+//            }
+//            if(p1.currentMon.getHealth()<=0){
+//                p1.switchOut();
+//                P1numberOfFaintedMons++;
+//            }
+//            if(p2.currentMon.getHealth()<=0){
+//                p2.switchOut();
+//                P2numberOfFaintedMons++;
+//            }
+//        }
 
 
 
@@ -480,11 +462,7 @@ public class Main extends JFrame {
 //        System.out.println(p2.currentMon);
     }
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
 
-	}
 
 
 }
---
+++ src/Button.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Button extends JButton {
+//    public String name;
+    public int num;
+    Button(String name,int nu){
+        super(name);
+        num=nu;
+
+    }
+}
